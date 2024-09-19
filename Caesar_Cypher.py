6f58588caae390ae53e29f00daba4fd5955665fc
Chaitanya Kumar <146342355+o-CodeBlitz-o@users.noreply.github.com>
2024-09-19 18:23:51 +0530

    used ASCII for list

--- Caesar_Cypher.py
+++ Caesar_Cypher.py
@@ -1,5 +1,8 @@
-Alphabets =['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
-'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+Alphabets =[]
+for i in range(65,123):
+        if (i >=91 and i<=96):
+                continue
+        Alphabets.append(chr(i))
 Name = input("What is your name?\n")
 print("\nHello "+ Name +"!!"+"\nWelcome to Caesar Cypher/Decypher")
 
