dfff9cca456062c5ff659ee9c427f2e00233de9a
Chaitanya Kumar <146342355+o-CodeBlitz-o@users.noreply.github.com>
2024-09-24 11:13:24 +0530

    Update BlackJack.py
    Finally Completed this game!!

--- BlackJack.py
+++ BlackJack.py
@@ -28,36 +28,41 @@ def Compare(U_Score,C_Score):
         return "You WON!!"
     else:
         return "You LOSE"
-
-User_Cards = []
-Computer_Cards = []
-User_Score = -1
-Computer_Score = -1
-Is_Game_Over = False
-
-for Num in range(2):
-    User_Cards.append(Deal_Card())
-    Computer_Cards.append(Deal_Card())
-while not Is_Game_Over :    
-    User_Score = Calculate_Score(User_Cards)
-    Computer_Score = Calculate_Score(Computer_Cards)
-    print(f" Your Cards : {User_Cards} , Current Score = {User_Score}")
-    print(f"Computer's First Cards : {Computer_Cards[0]}")
+        
+def Play_Game():
+    User_Cards = []
+    Computer_Cards = []
+    User_Score = -1
+    Computer_Score = -1
+    Is_Game_Over = False
     
-    if User_Score == 0 or Computer_Score == 0 or User_Score > 21:
-        Is_Game_Over = True
-    else:
-        User_should_deal = input("Type 'Y' to DRAW ANOTHER card, Type 'N' to PASS").upper()
-        if User_should_deal == 'Y':
-            User_Cards.append(Deal_card())
-        else:
+    for Num in range(2):
+        User_Cards.append(Deal_Card())
+        Computer_Cards.append(Deal_Card())
+    while not Is_Game_Over :    
+        User_Score = Calculate_Score(User_Cards)
+        Computer_Score = Calculate_Score(Computer_Cards)
+        print(f" Your Cards : {User_Cards} , Current Score = {User_Score}")
+        print(f"Computer's First Cards : {Computer_Cards[0]}")
+        
+        if User_Score == 0 or Computer_Score == 0 or User_Score > 21:
             Is_Game_Over = True
-            
-while Computer_Score != 0 and Computer_Score < 17:
-    Computer_Cards.append(Deal_Cards())
-    Computer_Score = Calculate_Score(Computer_Cards)
+        else:
+            User_should_deal = input("Type 'Y' to DRAW ANOTHER card, Type 'N' to PASS").upper()
+            if User_should_deal == 'Y':
+                User_Cards.append(Deal_card())
+            else:
+                Is_Game_Over = True
+                
+    while Computer_Score != 0 and Computer_Score < 17:
+        Computer_Cards.append(Deal_Cards())
+        Computer_Score = Calculate_Score(Computer_Cards)
+    
+    print(f"Your Final hand : {User_Cards}, Final Score : {User_Score}")
+    print(f"Opponent's Final hand : {Computer_Cards}, Opponent's Final Score : {Computer_Score}")
+    print(Compare(User_Score,Computer_Score))
 
-print(f"Your Final hand : {User_Cards}, Final Score : {User_Score}")
-print(f"Opponent's Final hand : {Computer_Cards}, Opponent's Final Score : {Computer_Score}")
-print(Compare(User_Score,Computer_Score))
+while (input("Do you want to Play a game of BlackJack?\nType 'Y' for Yes or 'N' for No").upper())== 'Y':
+    print("\n"*50)
+    Play_Game()
     
