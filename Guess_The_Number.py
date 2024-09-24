27b9be82937e7a8e527ec574034766ac48da4816
Chaitanya Kumar <146342355+o-CodeBlitz-o@users.noreply.github.com>
2024-09-24 20:33:35 +0530

    Update Guess_The_Number.py

--- Guess_The_Number.py
+++ Guess_The_Number.py
@@ -2,19 +2,16 @@ from random import randint
 
 HARD_TURN = 5
 EASY_TURN = 10
-turns = 0
-
-Player_Guess = int(input("What's your Guess : "))
 
 def Compare(P_Guess,C_Number):
     if P_Guess == C_Number:
         print( "Congratulations!! You guessed the right Number ;)")
     elif P_Guess > C_Number:
         print( "Too High, Try Again :/")
-        turns -= 1
+        return turns - 1
     else:
         print( "Too Low,Try Again :/")
-        turns -= 1
+        return turns - 1
 
 def Mode():
     Difficulty = (input("Choose a Difficulty : EASY or HARD :").upper())
@@ -24,14 +21,23 @@ def Mode():
         return EASY_TURN
     else :
         print("Wrong Input")
+        exit()
 
 def Game():
     print("Welcome to the Number Guessing Game :)")
     print("I'm thinking of a number between 1 to 100")
+    Chosen_Number = randint(1,100)
     turns = Mode()
-    print(f"You have {turns} attempts left")
+    
     Player_Guess = 0
     while Player_Guess != Chosen_Number:
+        print(f"You have {turns} attempts left")
         Player_Guess = int(input("What's your Guess : "))
-        Chosen_Number = randint(1,100)
-        Compare(P_Guess=Player_Guess,C_Number=Chosen_Number)
+        turns = Compare(Player_Guess,Chosen_Number,turns)
+        if turns == 0:
+            print("You've run out of guesses :/ ")
+            return
+        elif Player_Guess != Chosen_Number:
+            print("Guess Again.")
+
+Game()
