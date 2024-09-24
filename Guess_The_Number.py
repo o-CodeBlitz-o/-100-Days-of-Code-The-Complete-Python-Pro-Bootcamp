cb251cd6a54db1673ef627bd9d53ea63bc730d0d
Chaitanya Kumar <146342355+o-CodeBlitz-o@users.noreply.github.com>
2024-09-24 19:14:50 +0530

    Update Guess_The_Number.py

--- Guess_The_Number.py
+++ Guess_The_Number.py
@@ -1,20 +1,19 @@
 import random
 
-print("Welcome to the Number Guessing Game :)")
-print("I'm thinking of a number between 1 to 100")
-
 Num = []
 for i in range(1,101):
     Num.append(int(i))
 
 Chosen_Number = random.choice(Num)
+HARD_TURN = 5
+EASY_TURN = 10
 
 Player_Guess = int(input("What's your Guess : "))
 
-def Compare():
-    if Player_Guess == Chosen_Number:
+def Compare(P_Guess,C_Number):
+    if P_Guess == C_Number:
         return "Congratulations!! You guessed the right Number ;)"
-    elif Player_Guess > Chosen_Number:
+    elif P_Guess > C_Number:
         return "Too High, Try Again :/"
     else:
         return "Too Low,Try Again :/"
@@ -22,16 +21,17 @@ def Compare():
 def Mode():
     Difficulty = (input("Choose a Difficulty : EASY or HARD :").upper())
     if Difficulty == "HARD" :
-        while Number_of_Attempts >= 1:
-            Number_of_Attempts = 5
-            Compare()
-            Number_of_Attempts -= Number_of_Attempts
-            print(f"You have {Number_of_Attempts} attempts left to guess the number")
-    else:
-        while Number_of_Attempts >= 1:
-            Number_of_Attempts = 10
-            Compare()
-            Number_of_Attempts -= Number_of_Attempts
-            print(f"You have {Number_of_Attempts} attempts left to guess the number")
+        return HARD_TURN
+    elif Difficulty == "EASY" :
+        return EASY_TURN
+    else :
+        print("Wrong Input")
 
-Mode()
+
+print("Welcome to the Number Guessing Game :)")
+print("I'm thinking of a number between 1 to 100")
+turns = Mode()
+print(f"You have {turns} attempts left")
+Player_Guess = int(input("What's your Guess : "))
+
+Compare(P_Guess=Player_Guess,C_Number=Chosen_Number)
