cef363c09326854c0b31a70f20016d283a8b558d
Chaitanya Kumar <146342355+o-CodeBlitz-o@users.noreply.github.com>
2024-09-28 03:19:33 +0530

    Update HigherLower.py

--- HigherLower/HigherLower.py
+++ HigherLower/HigherLower.py
@@ -7,6 +7,12 @@ def Format_Data(Account):
     Account_Country = Account["country"]
     return (f"{Account_Name}, a {Account_Descr}, from {Account_Country}")
 
+def Check(User_Guess,Follower_A,Follower_B):
+        if Follower_A > Follower_B :
+            return User_Guess == "A"
+        else :
+            return User_Guess == "B"
+
 Account_A = random.choice(data)
 Account_B = random.choice(data)
 if Account_A == Account_B:
@@ -17,3 +23,6 @@ print(f"\nHello {Name}!!"+"\nWelcome to Higher Lower Game")
 
 print(f"Compare A against B :\n{Format_Data(Account_A)}\n\nVS\n\n{Format_Data(Account_B)}")
 Guess = input("Which one of these has MORE FOLLOWER COUNT? \n Type A or B : ").upper()
+
+A_Follower_Count = ["follower_count"]
+B_Follower_Count = ["follower_count"]
