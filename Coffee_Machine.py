880136c04664bf2c687f215e029f0f5a7264a12b
Chaitanya Kumar <146342355+o-CodeBlitz-o@users.noreply.github.com>
2024-09-30 20:10:59 +0530

    Add files via upload

---
+++ Coffee_Machine.py
@@ -0,0 +1,60 @@
+coffee_machine_data = {
+    "flavors": {
+        "Espresso": {
+            "ingredients": {
+                "water": 50,       
+                "milk": 0,         
+                "coffee_beans": 18, 
+                "sugar": 5         
+            },
+            "price": 40  # in INR
+        },
+        "Cappuccino": {
+            "ingredients": {
+                "water": 50,       
+                "milk": 100,       
+                "coffee_beans": 18, 
+                "sugar": 10        
+            },
+            "price": 60  # in INR
+        },
+        "Latte": {
+            "ingredients": {
+                "water": 30,       
+                "milk": 150,       
+                "coffee_beans": 18, 
+                "sugar": 8         
+            },
+            "price": 70  # in INR
+        },
+        "Americano": {
+            "ingredients": {
+                "water": 150,      
+                "milk": 0,         
+                "coffee_beans": 18, 
+                "sugar": 5         
+            },
+            "price": 50  # in INR
+        },
+        "Mocha": {
+            "ingredients": {
+                "water": 50,       
+                "milk": 100,       
+                "coffee_beans": 18, 
+                "chocolate_syrup": 20, 
+                "sugar": 15        
+            },
+            "price": 80  # in INR
+        },
+        "Macchiato": {
+            "ingredients": {
+                "water": 50,       
+                "milk": 50,        
+                "coffee_beans": 18, 
+                "sugar": 8         
+            },
+            "price": 65  # in INR
+        }
+    }
+}
+
