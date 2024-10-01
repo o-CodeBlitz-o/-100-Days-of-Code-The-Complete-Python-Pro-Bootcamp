eb74a25fc9ac4a1b82b4ff2d72dd18d6ec2965c0
Chaitanya Kumar <146342355+o-CodeBlitz-o@users.noreply.github.com>
2024-10-01 15:30:03 +0530

    Update Coffee_Machine.py

--- Coffee_Machine.py
+++ Coffee_Machine.py
@@ -1,60 +1,52 @@
 coffee_machine_data = {
     "flavors": {
-        "Espresso": {
-            "ingredients": {
-                "water": 50,       
-                "milk": 0,         
-                "coffee_beans": 18, 
-                "sugar": 5         
-            },
-            "price": 40  # in INR
-        },
-        "Cappuccino": {
-            "ingredients": {
-                "water": 50,       
-                "milk": 100,       
-                "coffee_beans": 18, 
-                "sugar": 10        
-            },
-            "price": 60  # in INR
-        },
-        "Latte": {
-            "ingredients": {
-                "water": 30,       
-                "milk": 150,       
-                "coffee_beans": 18, 
-                "sugar": 8         
-            },
-            "price": 70  # in INR
-        },
-        "Americano": {
-            "ingredients": {
-                "water": 150,      
-                "milk": 0,         
-                "coffee_beans": 18, 
-                "sugar": 5         
-            },
-            "price": 50  # in INR
-        },
-        "Mocha": {
-            "ingredients": {
-                "water": 50,       
-                "milk": 100,       
-                "coffee_beans": 18, 
-                "chocolate_syrup": 20, 
-                "sugar": 15        
-            },
-            "price": 80  # in INR
-        },
-        "Macchiato": {
-            "ingredients": {
-                "water": 50,       
-                "milk": 50,        
-                "coffee_beans": 18, 
-                "sugar": 8         
-            },
-            "price": 65  # in INR
-        }
+        "Espresso": [
+            ["water", 50],        
+            ["milk", 0],          
+            ["coffee_beans", 18],  
+            ["sugar", 5],         
+            40  # price in INR
+        ],
+        "Cappuccino": [
+            ["water", 50],        
+            ["milk", 100],        
+            ["coffee_beans", 18],  
+            ["sugar", 10],        
+            60  # price in INR
+        ],
+        "Latte": [
+            ["water", 30],        
+            ["milk", 150],        
+            ["coffee_beans", 18],  
+            ["sugar", 8],         
+            70  # price in INR
+        ],
+        "Americano": [
+            ["water", 150],       
+            ["milk", 0],          
+            ["coffee_beans", 18],  
+            ["sugar", 5],         
+            50  # price in INR
+        ],
+        "Mocha": [
+            ["water", 50],        
+            ["milk", 100],        
+            ["coffee_beans", 18],  
+            ["chocolate_syrup", 20], 
+            ["sugar", 15],        
+            80  # price in INR
+        ],
+        "Macchiato": [
+            ["water", 50],        
+            ["milk", 50],         
+            ["coffee_beans", 18],  
+            ["sugar", 8],         
+            65  # price in INR
+        ]
     }
 }
+
+
+}
+
 
